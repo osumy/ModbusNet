f6656c2a4b15f50335090049bafbe7e160bb4f78
osumy <amirali40223@gmail.com>
2025-11-06 16:44:15 +0330

    feat: new file IModbusTransport

---
+++ ModbusNet/Transport/IModbusTransport.cs
@@ -0,0 +1,10 @@
+﻿namespace ModbusNet.Transport
+{
+    public interface IModbusTransport : IDisposable
+    {
+        byte[] SendRequest(byte[] request);
+        bool IsConnected { get; }
+        void Connect();
+        void Disconnect();
+    }
+}
