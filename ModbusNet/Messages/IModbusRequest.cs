df2a40a57d53b959ee9489a5fb67c5665e70f9bc
osumy <amirali40223@gmail.com>
2025-11-07 15:05:02 +0330

    feat: add new file IModbusRequest

---
+++ ModbusNet/Messages/IModbusRequest.cs
@@ -0,0 +1,15 @@
+﻿using ModbusNet.Core.Messages;
+
+namespace ModbusNet.Messages
+{
+    /// <summary>
+    ///     Methods specific to a modbus request message.
+    /// </summary>
+    public interface IModbusRequest : IModbusMessage
+    {
+        /// <summary>
+        ///     Validate the specified response against the current request.
+        /// </summary>
+        void ValidateResponse(IModbusMessage response);
+    }
+}
