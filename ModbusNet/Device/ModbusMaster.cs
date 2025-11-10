9024e98432a2833fd5914c81db3e8395f0665025
osumy <amirali40223@gmail.com>
2025-11-10 12:13:45 +0330

    refactor: adding all defined public function codes

--- ModbusNet/Device/ModbusMaster.cs
+++ ModbusNet/Device/ModbusMaster.cs
@@ -1,12 +1,9 @@
-﻿using ModbusNet.Messages.Requests;
-
+﻿using ModbusNet.Device.Validation;
+using ModbusNet.Messages.Requests;
 using ModbusNet.Transport;
 
 namespace ModbusNet.Device
 {
-    /// <summary>
-    ///     Modbus master device
-    /// </summary>
     public class ModbusMaster : IModbusMaster
     {
         private readonly IModbusTransport _transport;
@@ -17,89 +14,258 @@ namespace ModbusNet.Device
             _transport = transport;
         }
 
-        /// <summary>
-        ///    Reads contiguous block of holding registers.
-        /// </summary>
-        /// <param name="slaveId">Address of device to read values from.</param>
-        /// <param name="startAddress">Address to begin reading.</param>
-        /// <param name="numberOfPoints">Number of holding registers to read.</param>
-        /// <returns>Holding registers status.</returns>
-        public ushort[] ReadHoldingRegisters(byte slaveId, ushort startAddress, ushort numberOfPoints)
-        {
-            ThrowIfDisposed();
 
-            if (numberOfPoints < 1 || numberOfPoints > 125)
-                throw new ArgumentOutOfRangeException(nameof(numberOfPoints), "Must be 1-125 for Read Holding Registers");
-
-            var request = new ReadHoldingRegistersRequest(0x03, slaveId, startAddress, numberOfPoints);
-
-            //return PerformReadRegisters(request);
-            request.Serialize();
-            var responseData = SendRequestWithRetry(request.MessageFrame);
-            //var response = ReadHoldingRegistersResponse.Deserialize(slaveId, responseData);
-            return responseData;
-        }
-
-        public ushort ReadHoldingRegister(byte slaveId, ushort address)
-        {
-            ThrowIfDisposed();
-
-            var registers = ReadHoldingRegisters(slaveId, address, 1);
-            return registers[0];
-        }
-
-        /// <summary>
-        ///    Writes a single holding register.
-        /// </summary>
-        /// <param name="slaveId">Address of the device to write to.</param>
-        /// <param name="registerAddress">Address to write.</param>
-        /// <param name="value">Value to write.</param>
-        public void WriteSingleRegister(byte slaveId, ushort registerAddress, ushort value)
-        {
-            //ThrowIfDisposed();
-
-            //var request = new WriteSingleRegisterRequest(slaveId, registerAddress, value);
-            //var responseData = SendRequestWithRetry(request.Serialize());
-            //var response = WriteSingleRegisterResponse.Deserialize(slaveId, responseData);
-
-            //if (response.Address != registerAddress || response.Value != value)
-            //    throw new InvalidOperationException("Write register verification failed");
-        }
-
-        //private ushort[] PerformReadRegisters(ReadHoldingRegistersRequest request)
+        //public ushort[] ReadHoldingRegisters(byte slaveId, ushort startAddress, ushort numberOfPoints)
         //{
-        //    ReadHoldingRegistersResponse response =
-        //            Transport.UnicastMessage<ReadHoldingInputRegistersResponse>(request);
+        //    ThrowIfDisposed();
 
-        //    var response = ReadHoldingRegistersResponse.Deserialize(slaveId, responseData);
-        //    return response.Registers;
+        //    if (numberOfPoints < 1 || numberOfPoints > 125)
+        //        throw new ArgumentOutOfRangeException(nameof(numberOfPoints), "Must be 1-125 for Read Holding Registers");
 
-        //    return response.Data.Take(request.NumberOfPoints).ToArray();
+        //    var request = new ReadHoldingRegistersRequest(0x03, slaveId, startAddress, numberOfPoints);
+
+        //    //return PerformReadRegisters(request);
+        //    request.Serialize();
+        //    var responseData = SendRequestWithRetry(request.MessageFrame);
+        //    //var response = ReadHoldingRegistersResponse.Deserialize(slaveId, responseData);
+        //    return responseData;
         //}
 
-        private ushort[] SendRequestWithRetry(byte[] request)
+        //private ushort[] SendRequestWithRetry(byte[] request)
+        //{
+        //    var retries = 3;
+        //    var delayBetweenRetriesMs = 100;
+
+        //    for (int attempt = 0; attempt <= retries; attempt++)
+        //    {
+        //        try
+        //        {
+        //            return _transport.SendRequest(request);
+        //        }
+        //        catch (TimeoutException) when (attempt < retries)
+        //        {
+        //            Thread.Sleep(delayBetweenRetriesMs);
+        //        }
+        //    }
+
+        //    throw new TimeoutException($"Request failed after {retries + 1} attempts");
+        //}
+
+
+        #region Bit Access - Discrete Inputs
+
+        public bool[] ReadDiscreteInputs(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Bit Access - Coils
+
+        public bool[] ReadCoils(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteSingleCoil(byte slaveAddress, ushort coilAddress, bool value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteMultipleCoils(byte slaveAddress, ushort startAddress, bool[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Word Access - Input Registers
+
+        public ushort[] ReadInputRegisters(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Word Access - Holding Registers
+
+        public ushort[] ReadMultipleHoldingRegisters(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteSingleHoldingRegister(byte slaveAddress, ushort registerAddress, ushort value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteMultipleHoldingRegisters(byte slaveAddress, ushort startAddress, ushort[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ushort[] ReadWriteMultipleRegisters(byte slaveAddress, ushort startReadAddress, ushort numberOfPointsToRead, ushort startWriteAddress, ushort[] writeData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MaskWriteRegister(byte slaveAddress, ushort registerAddress, ushort andMask, ushort orMask)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ushort[] ReadFifoQueue(byte slaveAddress, ushort fifoPointerAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region File Record Access
+
+        public byte[] ReadFileRecord(byte slaveAddress, ushort fileNumber, ushort startingAddress, ushort numberOfRegisters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteFileRecord(byte slaveAddress, ushort fileNumber, ushort startingAddress, byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Diagnostics (Serial Only)
+
+        public byte ReadExceptionStatus(byte slaveAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ushort[] Diagnostics(byte slaveAddress, ushort subFunctionCode, ushort[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public (ushort Status, ushort EventCount) GetCommunicationEventCounter(byte slaveAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public (ushort Status, ushort EventCount, ushort MessageCount, byte[] Events) GetCommunicationEventLog(byte slaveAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] ReportServerId(byte slaveAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Other Function Codes
+
+        public byte[] ReadDeviceIdentification(byte slaveAddress, byte objectId, byte meiType)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Validation and Disposal
+
+        /// <summary>
+        /// Validates the operation parameters based on the validation type and checks if the instance is disposed.
+        /// </summary>
+        /// <param name="validationType">The type of Modbus operation to validate.</param>
+        /// <param name="numberOfPoints">The number of points to read/write.</param>
+        /// <param name="data">The data array to validate (optional).</param>
+        /// <param name="argumentName">The name of the argument being validated.</param>
+        protected void Validate(ValidationType validationType, ushort numberOfPoints = 0, Array data = null, string argumentName = "data")
         {
             ThrowIfDisposed();
 
-            var retries = 3;
-            var delayBetweenRetriesMs = 100;
-
-            for (int attempt = 0; attempt <= retries; attempt++)
+            switch (validationType)
             {
-                try
-                {
-                    return _transport.SendRequest(request);
-                }
-                catch (TimeoutException) when (attempt < retries)
-                {
-                    Thread.Sleep(delayBetweenRetriesMs);
-                }
-            }
+                case ValidationType.ReadCoils:
+                case ValidationType.ReadDiscreteInputs:
+                    ValidateNumberOfPoints(argumentName, numberOfPoints, ValidationLimits.MaxReadCoils);
+                    break;
 
-            throw new TimeoutException($"Request failed after {retries + 1} attempts");
+                case ValidationType.ReadHoldingRegisters:
+                case ValidationType.ReadInputRegisters:
+                    ValidateNumberOfPoints(argumentName, numberOfPoints, ValidationLimits.MaxReadHoldingRegisters);
+                    break;
+
+                case ValidationType.WriteMultipleCoils:
+                    ValidateData(argumentName, data, ValidationLimits.MaxWriteMultipleCoils);
+                    break;
+
+                case ValidationType.WriteMultipleRegisters:
+                    ValidateData(argumentName, data, ValidationLimits.MaxWriteMultipleRegisters);
+                    break;
+
+                case ValidationType.ReadWriteMultipleRegisters:
+                    ValidateNumberOfPoints("numberOfPointsToRead", numberOfPoints, ValidationLimits.MaxReadWriteMultipleRegistersRead);
+                    if (data != null)
+                    {
+                        ValidateData("writeData", data, ValidationLimits.MaxReadWriteMultipleRegistersWrite);
+                    }
+                    break;
+
+                case ValidationType.WriteFileRecord:
+                    ValidateMaxData(argumentName, data, ValidationLimits.MaxWriteFileRecord);
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(validationType), $"Unknown validation type: {validationType}");
+            }
         }
 
-        private void ThrowIfDisposed()
+        private static void ValidateData(string argumentName, Array data, int maxDataLength)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (data.Length == 0 || data.Length > maxDataLength)
+            {
+                string msg = $"The length of argument {argumentName} must be between 1 and {maxDataLength} inclusive.";
+                throw new ArgumentException(msg);
+            }
+        }
+
+        private static void ValidateMaxData(string argumentName, Array data, int maxDataLength)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            if (data.Length > maxDataLength)
+            {
+                string msg = $"The length of argument {argumentName} must not be greater than {maxDataLength}.";
+                throw new ArgumentException(msg);
+            }
+        }
+
+        private static void ValidateNumberOfPoints(string argumentName, ushort numberOfPoints, ushort maxNumberOfPoints)
+        {
+            if (numberOfPoints < 1 || numberOfPoints > maxNumberOfPoints)
+            {
+                string msg = $"Argument {argumentName} must be between 1 and {maxNumberOfPoints} inclusive.";
+                throw new ArgumentException(msg);
+            }
+        }
+
+        /// <summary>
+        /// Validates that the instance has not been disposed.
+        /// </summary>
+        protected void ThrowIfDisposed()
         {
             if (_disposed)
                 throw new ObjectDisposedException(GetType().Name);
@@ -113,5 +279,7 @@ namespace ModbusNet.Device
                 _disposed = true;
             }
         }
+
+        #endregion
     }
 }
