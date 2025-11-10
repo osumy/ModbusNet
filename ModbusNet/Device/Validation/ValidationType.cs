cdfd5a3429050f5b0808f753e25679851e2c282a
osumy <amirali40223@gmail.com>
2025-11-10 12:12:44 +0330

    feat: add validation enums
    used in ModbusMaster validation methods

---
+++ ModbusNet/Device/Validation/ValidationType.cs
@@ -0,0 +1,14 @@
+﻿namespace ModbusNet.Device.Validation
+{
+    public enum ValidationType
+    {
+        ReadCoils,
+        ReadDiscreteInputs,
+        ReadHoldingRegisters,
+        ReadInputRegisters,
+        WriteMultipleCoils,
+        WriteMultipleRegisters,
+        ReadWriteMultipleRegisters,
+        WriteFileRecord
+    }
+}
