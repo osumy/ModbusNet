8c1061e91cfe0fcd94233435e635a8ac1b78a0e1
osumy <amirali40223@gmail.com>
2025-11-10 10:35:26 +0330

    refactor: update ModbusSettings

--- ModbusNet/ModbusSettings.cs
+++ ModbusNet/ModbusSettings.cs
@@ -1,10 +1,18 @@
-﻿namespace ModbusNet
+﻿using Modbus.Models.Enums;
+using System.IO.Ports;
+
+namespace ModbusNet
 {
     public class ModbusSettings
     {
         public static ModbusSettings Default => new ModbusSettings();
 
 
+        public byte retryCount { get; set; } = 3;
+        public int retryDelayMs { get; set; } = 50;
+
+
+        #region Serial Port Settings
         public TimeSpan Timeout
         {
             get => responseTimeout;
@@ -17,16 +25,20 @@
         public TimeSpan responseTimeout { get; set; } = TimeSpan.FromSeconds(2);
         public TimeSpan requestTimeout { get; set; } = TimeSpan.FromSeconds(2);
 
+        public PortName PortName { get; set; } = PortName.COM3;
+        public int BaudRate { get; set; } = 9600;
+        public Parity Parity { get; set; } = Parity.Even;
+        public DataBits DataBits { get; set; } = DataBits.db7;
+        public StopBits StopBits { get; set; } = StopBits.One;
+        #endregion
 
-        public byte retryCount { get; set; } = 3;
-        public int retryDelayMs { get; set; } = 50;
-
-
+        #region ASCII/RTU Specific Settings
         public string AsciiStartDelimiter { get; set; } = ":";
         public string AsciiEndDelimiter { get; set; } = "\r\n";
 
 
         public int RtuInterCharTimeoutMs { get; set; } = 2;
         public int RtuInterFrameTimeoutMs { get; set; } = 5;
+        #endregion
     }
 }
