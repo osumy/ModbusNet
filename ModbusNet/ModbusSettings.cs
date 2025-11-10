15f3cd1d8ade69316222c8b8b2db2e8549592dd0
osumy <amirali40223@gmail.com>
2025-11-10 10:22:10 +0330

    refactor: update ModbusSettings
    + Timeout, responseTimeout and requestTimeout

--- ModbusNet/ModbusSettings.cs
+++ ModbusNet/ModbusSettings.cs
@@ -4,12 +4,27 @@
     {
         public static ModbusSettings Default => new ModbusSettings();
 
-        public TimeSpan Timeout { get; set; } = TimeSpan.FromSeconds(5);
-        public byte Retries { get; set; } = 3;
-        public int DelayBetweenRetriesMs { get; set; } = 100;
+
+        public TimeSpan Timeout
+        {
+            get => responseTimeout;
+            set
+            {
+                responseTimeout = value;
+                requestTimeout = value;
+            }
+        }
+        public TimeSpan responseTimeout { get; set; } = TimeSpan.FromSeconds(2);
+        public TimeSpan requestTimeout { get; set; } = TimeSpan.FromSeconds(2);
+
+
+        public byte retryCount { get; set; } = 3;
+        public int retryDelayMs { get; set; } = 50;
+
 
         public string AsciiStartDelimiter { get; set; } = ":";
         public string AsciiEndDelimiter { get; set; } = "\r\n";
+
 
         public int RtuInterCharTimeoutMs { get; set; } = 2;
         public int RtuInterFrameTimeoutMs { get; set; } = 5;
