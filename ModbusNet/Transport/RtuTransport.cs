b46c9dc18d3d25c2f064ca69d3036fb463060a1d
osumy <amirali40223@gmail.com>
2025-11-12 17:03:13 +0330

    refactor: RtuTransport cleanup

--- ModbusNet/Transport/RtuTransport.cs
+++ ModbusNet/Transport/RtuTransport.cs
@@ -24,76 +24,22 @@ namespace ModbusNet.Transport
         }
 
 
-        public byte[] SendRequest(byte[] request)
+        public override ModbusResponse SendRequestReceiveResponse(byte[] request)
         {
-            ThrowIfDisposed();
-            //// اضافه کردن CRC
-            //var crc = ErrorCheckCalculator.ComputeCrc(request);
-            //var frame = new byte[request.Length + 2];
-            //Array.Copy(request, 0, frame, 0, request.Length);
-            //frame[request.Length] = (byte)(crc & 0xFF);
-            //frame[request.Length + 1] = (byte)((crc >> 8) & 0xFF);
-
-            //// ارسال
-            //_serialPort.Write(frame, 0, frame.Length);
-
-            //// دریافت پاسخ
-            return ReceiveResponse(request[0]); // slaveId
+            throw new NotImplementedException();
+        }
+        public override void SendRequestIgnoreResponse(byte[] request)
+        {
+            throw new NotImplementedException();
         }
 
-        private byte[] ReceiveResponse(byte expectedSlaveId)
+        public override void ChecksumsMatch(byte[] rawMessage, byte[] ErrorCheckBytes)
         {
-            // خواندن هدر (SlaveId + FunctionCode)
-            var header = new byte[2];
-            ReadBytes(header, 0, 2);
-
-            if (header[0] != expectedSlaveId)
-                throw new InvalidOperationException("Slave ID mismatch");
-
-            // تشخیص طول داده بر اساس Function Code
-            int dataLength = GetExpectedDataLength(header[1]);
-            var response = new byte[2 + dataLength + 2]; // header + data + crc
-
-            // کپی هدر
-            response[0] = header[0];
-            response[1] = header[1];
-
-            // خواندن بقیه داده
-            ReadBytes(response, 2, dataLength + 2); // data + crc
-
-            // بررسی CRC
-            var dataWithoutCrc = response.Take(response.Length - 2).ToArray();
-            var receivedCrc = (ushort)(response[response.Length - 2] | (response[response.Length - 1] << 8));
-            var calculatedCrc = ErrorCheckUtility.ComputeCrc(dataWithoutCrc);
-
-            //if (receivedCrc != calculatedCrc) // TODO
-            //    throw new InvalidOperationException("CRC check failed");
-
-            return dataWithoutCrc;
+            throw new NotImplementedException();
         }
-
-        private int GetExpectedDataLength(byte functionCode)
+        public override byte[] BuildRequest(byte slaveAddress, byte[] pdu)
         {
-            return functionCode switch
-            {
-                0x01 or 0x02 => 1, // Read Coils/Inputs - byte count
-                0x03 or 0x04 => 1, // Read Registers - byte count  
-                0x05 or 0x06 => 4, // Write Single
-                0x0F or 0x10 => 2, // Write Multiple
-                _ => throw new NotSupportedException($"Function code 0x{functionCode:X2} not supported")
-            };
-        }
-
-        private void ReadBytes(byte[] buffer, int offset, int count)
-        {
-            int totalRead = 0;
-            while (totalRead < count)
-            {
-                int read = _serialPort.Read(buffer, offset + totalRead, count - totalRead);
-                if (read == 0)
-                    throw new TimeoutException("Read timeout");
-                totalRead += read;
-            }
+            throw new NotImplementedException();
         }
 
         private void ThrowIfDisposed()
@@ -111,24 +57,5 @@ namespace ModbusNet.Transport
             }
         }
 
-        public ushort[] SendRequestWithRetry16A(byte[] request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override byte[] BuildRequest(byte slaveAddress, byte[] pdu)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override ModbusResponse SendRequestReceiveResponse(byte[] request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void ChecksumsMatch(byte[] rawMessage, byte[] ErrorCheckBytes)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
