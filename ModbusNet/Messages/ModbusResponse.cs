1e36904daa17bf8483ca219a35da1b7c6a226aaa
osumy <amirali40223@gmail.com>
2025-11-12 16:19:43 +0330

    feat: ModbusResponse

---
+++ ModbusNet/Messages/ModbusResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace ModbusNet.Messages
+{
+    public class ModbusResponse
+    {
+        public ushort[] Registers { get; private set; }
+
+        public ModbusResponse(ushort[] rgisters)
+        {
+            Registers = rgisters;
+        }
+
+    }
+}
